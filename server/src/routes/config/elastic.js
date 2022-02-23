283278a3f553e5efcb9d88a1c18dbb3e38024dd6
RyanV-Souza <ryanvsouza99@gmail.com>
2022-02-23 09:53:56 -0300

    Create elastic conn

---
+++ server/src/routes/config/elastic.js
@@ -0,0 +1,9 @@
+const {Client} = require('@elastic/elasticsearch')
+
+const client = new Client({
+	node: 'http://localhost:9200',
+	maxRetries: 5,
+	requestTimeout: 60000
+})
+
+module.exports = client

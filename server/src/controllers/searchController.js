e225da341c6191f5daf7162ccdb9ad512a2426d3
RyanV-Souza <ryanvsouza99@gmail.com>
2022-02-23 09:57:53 -0300

    Create search controller

---
+++ server/src/controllers/searchController.js
@@ -0,0 +1,23 @@
+const client = require('../routes/config/elastic')
+
+exports.get = (req, res, next) => {
+	const text = req.query.text
+
+	client.search({
+		body: {
+			query: {
+				fuzzy: {
+					nmLogradouro: {
+						value: text
+					}
+				}
+			}
+		}
+	})
+	.then(response => {
+		return res.json(response.body.hits)
+	})
+	.catch(err => {
+		return res.status(500).json({"message": err})
+	})
+}
